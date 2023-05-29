3dc183c0c634a6880fed86c5c9987c312603984c
alvesgeraldo <jgalvesb@gmail.com>
2023-05-29 16:45:37 -0300

    Ajustes de configuraçao do index.php

--- index.php
+++ index.php
@@ -6,18 +6,18 @@
 
   $uri = explode( "/" , $uri);
 
-  if ( ( isset($uri[1]) && $uri[1] != 'api') || (isset($uri[2]) && $uri[2] != 'v1')) {
+  if((isset($uri[1]) && $uri[1] != 'api') || (isset($uri[2]) && $uri[2] != 'v1')){
     header("HTTP/1.1 404 Not Found");
     exit();
-  } else if ( ( isset($uri[3]) && $uri[3] != 'user') || (isset($uri[4]))) {
-    header("HTTP/1.1 404 Not Found");
-    exit(); 
+  } else if ((isset($uri[3]) && $uri[3] != 'user') || !isset($uri[4])) {
+      header("HTTP/1.1 404 Not Found");
+      exit();
   }
 
   require ROOT_PATH . "Controller/API/UserController.php";
 
   // $user = new UserController();
 
-  // $methodName = $uri[4].'Action';
+  $methodName = $uri[4].'Action';
 
-  // $user->{$methodName}();
+  $user->{$methodName}();
